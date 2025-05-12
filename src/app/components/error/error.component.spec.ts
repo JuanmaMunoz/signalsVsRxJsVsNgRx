1e984d1ed3f5d9ac67b80c292f4b4996551ad826
Juanma Muñoz Gonzalez <jmmugoz@gmail.com>
2025-05-12 18:19:21 +0200

    second commit test

--- src/app/components/error/error.component.spec.ts
+++ src/app/components/error/error.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { HttpErrorResponse } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
 import { ErrorComponent } from './error.component';
 
-xdescribe('ErrorComponent', () => {
+describe('ErrorComponent', () => {
   let component: ErrorComponent;
   let fixture: ComponentFixture<ErrorComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ErrorComponent],
+      imports: [ErrorComponent, TranslateModule.forRoot()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorComponent);
@@ -18,5 +20,21 @@ xdescribe('ErrorComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should to exist error', () => {
+    component.error = { error: { message: 'Error Server', code: 500 } } as HttpErrorResponse;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const error = compiled.querySelector('.error');
+    expect(error?.textContent).toContain('Error Server');
+  });
+
+  it('should to use default error', () => {
+    component.error = null;
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement as HTMLElement;
+    const error = compiled.querySelector('.error');
+    expect(error?.textContent).toContain('Internal Server Error');
   });
 });
